927913dd4a93d3da0ab149283ed96a2d834762c5
unknown <vini.silveirasg@hotmail.com>
2025-04-28 23:00:14 -0300

    ci-cd

--- cypress/e2e/Busca.cy.js
+++ cypress/e2e/Busca.cy.js
@@ -3,19 +3,24 @@
 describe('US-001-Funcionalidades: Busca de filmes', () => {
   beforeEach(() => {
     cy.visit('/')
-  })
-});
+  });
+  
+  afterEach(() => {
+    cy.screenshot()
+  });
 
-it('Deve buscar filmes com sucesso', () => {
-  cy.get('#search-input').type('Inception')
-  cy.get('#search-button').click()
-  cy.get('#results-section').should('contain', 'Inceptiton')
-});
 
-it('Deve buscar filmes com sucesso de uma lista', () => {
-  cy.fixture('Filmes').then((Filmes) =>{
-    cy.get('#search-input').type(Filmes[1].título)
-    cy.get('#search-button').click()
-    cy.get('#results-section').should('contain', Filmes[1].título)
-  })
+  it('Deve buscar filmes com sucesso', () => {
+    cy.get('#search-input').type('Inception') 
+    cy.get('#search-button').click() 
+    cy.get('#results-section').should('contain', 'Inception') 
+  });
+
+  it('Deve buscar filmes com sucesso de uma lista', () => {
+    cy.fixture('Filmes').then((Filmes) => {
+      cy.get('#search-input').type(Filmes[1].título) 
+      cy.get('#search-button').click() 
+      cy.get('#results-section').should('contain', Filmes[1].título) 
+    })
+  });
 });
